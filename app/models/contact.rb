c86f40b0b4f30eb2bcacb6c811b89cea20f397fc
Carlos Bertomeu <cbertomeu18@gmail.com>
2021-09-30 21:31:58 +0200

    Improve validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,6 @@ class Contact < ApplicationRecord
   belongs_to :user
   has_many :contact_histories, dependent: :destroy
   validates_presence_of :first_name, :last_name, :email, :phone_number
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { scope: :user_id }
-  validates :phone_number, numericality: { greater_than: 999, less_than: 9999999999999, only_integer: true }
+  validates :email, uniqueness: { scope: :user_id }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }
+  validates :phone_number, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email, :password
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }
   has_many :contacts, dependent: :destroy
   has_many :contact_histories
 end
