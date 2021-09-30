5f072240eed66a96f9bef88b2bf4ce4cea6ee88c
Carlos Bertomeu <cbertomeu18@gmail.com>
2021-10-01 00:45:37 +0200

    Fix contact history validation

--- app/models/contact_history.rb
+++ app/models/contact_history.rb
@@ -2,6 +2,6 @@ class ContactHistory < ApplicationRecord
   belongs_to :contact
   belongs_to :user
   validates_presence_of :first_name,:last_name,:email, :phone_number, :state
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone_number, numericality: { greater_than: 999, less_than: 9999999999999, only_integer: true }
+  validates :email, uniqueness: { scope: :user_id }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/ }
+  validates :phone_number, numericality: { only_integer: true }
 end
