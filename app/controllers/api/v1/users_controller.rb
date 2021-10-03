4dd95efa5e25554b86557c7a63b44f495737ddb4
Carlos Bertomeu <cbertomeu18@gmail.com>
2021-10-03 18:21:38 +0200

    Fix return json object

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: @user.as_json(only: [:id, :email, :created_at, :updated_at]), status: :created
     else
       response = Hash.new 
       response['errors'] = @user.errors
@@ -30,7 +30,7 @@ class Api::V1::UsersController < ApplicationController
 
     if @user && @user.authenticate(params[:password])
       token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+      render json: {user: @user.as_json(only: [:id, :email, :created_at, :updated_at]), token: token}
     else
       response = Hash.new
       response['errors'] = {"invalid":["email or password"]}
